2c4d74936b6261b1ae6e670b826c9a11f7683a97
sudhanshu-acl <sudhanshu-acl>
2026-04-21 16:21:43 +0530

    feat: implement authentication module and configure CORS middleware in main application

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from app.modules.todos.api.v1.router import router as todo_router
 from app.core.docs import description, tags_metadata, contact_info, license_info, servers
 import os
 import platform
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Annotated
 
 Base.metadata.create_all(bind=engine)
 
@@ -18,6 +20,20 @@ app = FastAPI(
     openapi_tags=tags_metadata,
     servers=servers,
 )
+
+origins = [
+    "http://localhost:8000",  # frontend URL
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,   # or ["*"] for testing
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/", tags=["root"])
 def read_root():
